4c39a336f934f3fb10e46501da62348a4f19d73d
bagrkonstantin <costa.spy@yandex.ru>
2023-03-27 13:20:00 +0300

    Initial commit

---
+++ src/main/java/org/example/CycleList.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import org.testng.internal.collections.Pair;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class CycleList<T> {
+    private List<T> list;
+
+    public CycleList() {
+        list = new ArrayList<>();
+    }
+
+    public CycleList(List<T> list) {
+        this.list = new ArrayList<>(list.size());
+        this.list.addAll(list);
+
+    }
+
+    public T get(int index) {
+        return list.get((index + list.size()) % list.size());
+    }
+
+    public CycleList<T> moveBy(int n) {
+        List<T> newList = new ArrayList<>(list.size());
+        for (int i = 0; i < list.size(); i++) {
+            newList.add(get(i + n + list.size()));
+        }
+        return new CycleList<>(newList);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof CycleList) || list.size() != ((CycleList<?>) obj).list.size()) {
+            return false;
+        }
+        for (int i = 0; i < list.size(); i++) {
+            if (!list.get(i).equals(((CycleList<?>) obj).list.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        for (T item : list) {
+            sb.append(item);
+            sb.append(", ");
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    public int size() {
+        return list.size();
+    }
+}
---
+++ src/test/java/CycleListTester.java
@@ -0,0 +1,92 @@
+import org.example.CycleList;
+import org.junit.jupiter.api.Order;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+
+import static org.testng.AssertJUnit.*;
+
+public final class CycleListTester {
+    private final CycleList<Integer> cycleList1 = new CycleList(new ArrayList<Integer>() {
+        {
+            add(1);
+            add(2);
+            add(3);
+        }
+    });
+
+    private final CycleList cycleList2 = new CycleList();
+
+    private final CycleList<Integer> cycleList3 = new CycleList(new ArrayList<Integer>() {
+        {
+            add(2);
+            add(3);
+            add(1);
+        }
+    });
+
+    private final CycleList<Integer> cycleList4 = new CycleList(new ArrayList<Integer>() {
+        {
+            add(2);
+            add(1);
+        }
+    });
+
+    private final CycleList<String> cycleList5 = new CycleList(new ArrayList<String>() {
+        {
+            add("a");
+            add("Bab");
+        }
+    });
+
+
+    @Test
+    @Order(1)
+    public void testEqualsEqual() {
+        assertEquals(cycleList1, cycleList1);
+    }
+
+    @Test
+    @Order(2)
+    public void noMatchingSizesTest() {
+        assertTrue(!cycleList1.equals(cycleList2));
+    }
+
+    @Test
+    @Order(3)
+    public void testEqualsDifferentTypes() {
+        assertTrue(!cycleList1.equals(cycleList2));
+    }
+
+
+    @Test
+    @Order(4)
+    public void moveByTest() {
+        assertEquals(cycleList1.moveBy(1), cycleList3);
+    }
+
+    @Test
+    @Order(5)
+    public void cyclicTest() {
+        for (int i = 0; i < cycleList1.size(); i++) {
+            assertEquals(cycleList1.get(i), cycleList1.get(i + cycleList1.size()));
+
+        }
+    }
+
+    @Test
+    @Order(6)
+    public void cyclicTestNegative() {
+        for (int i = 0; i < cycleList1.size(); i++) {
+            assertEquals(cycleList1.get(i), cycleList1.get(i - cycleList1.size()));
+
+        }
+    }
+
+
+    @Test
+    @Order(7)
+    public void toStringEqualsTest() {
+        assertEquals(cycleList1.toString(), "[1, 2, 3, ]");
+    }
+}
